715535258302264075b2e62c157ef01bb1bd3aaf
Kishore Karra <kishore.0331@gmail.com>
2025-08-30 01:12:08 +0530

    Add files via upload

---
+++ Lab/Week 5/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+	<div className="calculator">
+      <h1>Simple React Calculator</h1>
+	  </div>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
